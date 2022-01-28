b89cb3800ead22fb147f1538f9a6482c76e10f4a
Lf Laguardia <laguardialuis@gmail.com>
2022-01-28 12:16:26 -0300

    First update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,26 @@
-import streamlit as sl
+from matplotlib.pyplot import bar
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-sl.write("Hello person.")
+df = pd.read_csv("heroes_information.csv")
+
+st.sidebar.header("Title of the side bar")
+
+st.write("Hello person.")
+
+number = st.slider("Select a number", 0, 10, 5, 1)
+st.write(f"""
+*Oh I see...*
+
+I also like {number} a lot.
+""")
+
+st.write("# Very big table here")
+st.write(df)
+
+st.bar_chart(df['Weight'])
+
+fig = px.bar(df.sample(10), x="name", y="Height", color="Eye color")
+st.plotly_chart(fig)
+
